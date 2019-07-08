c7b635e7d428fe85586c1eedaec0389d8a8298d3
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-08 10:06:01 -0600

    Add method for average age

--- app/models/astronaut.rb
+++ app/models/astronaut.rb
@@ -3,4 +3,9 @@ class Astronaut < ApplicationRecord
 
   has_many :missions, through: :astronaut_missions
   has_many :astronaut_missions
+
+  def self.avg_age
+    Astronaut.average(:age).to_i
+  end
+
 end
