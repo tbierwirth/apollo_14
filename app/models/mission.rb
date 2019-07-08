428896732fa055ecc0f61a2598f842720bfda8a8
Tyler Bierwirth <tylerbierwirth@gmail.com>
2019-07-08 09:13:44 -0600

    Fix relations

--- app/models/mission.rb
+++ app/models/mission.rb
@@ -1,7 +1,6 @@
 class Mission < ApplicationRecord
   validates_presence_of :title, :time_in_space
 
-  has_many :missions
   has_many :astronaut_missions
-  has_many :astronatus
+  has_many :astronauts, through: :astronaut_missions
 end
